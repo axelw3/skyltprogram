bde35925d48e9cf5db18a8f8d3e543bd8aa07922
axelw3 <axelw3@outlook.com>
2025-03-03 20:33:33 +0100

    Uppdatera rendering

--- src/render.ts
+++ src/render.ts
@@ -1,26 +1,24 @@
 import { SignRenderer } from "../packages/skyltrendering/src/browser.js";
 import CONFIG from "./config.js"
 
-const tratex = new FontFace(
-    "Tratex",
-    "url(font/TratexSvart-Regular.otf)",
-);
+let renderer = new SignRenderer(CONFIG);
 
-const tratexVersal = new FontFace(
-    "TratexVersal",
-    "url(font/TRATEXPOSVERSAL-POSVERSAL.otf)",
-);
-
-document.fonts.add(tratex);
-document.fonts.add(tratexVersal);
-
-Promise.all([tratex.load(), tratexVersal.load()]).then(() => {
+Promise.all([
+    renderer.registerFont(
+        "Tratex",
+        "font/TratexSvart-Regular.otf"
+    ),
+    renderer.registerFont(
+        "TratexVersal",
+        "font/TRATEXPOSVERSAL-POSVERSAL.otf"
+    )
+]).then(() => {
     let url = new URL(window.location.href);
     if(!url.searchParams.has("data")) return;
 
     let data = JSON.parse(url.searchParams.get("data") || "");
 
-    new SignRenderer(CONFIG).render(data).then((canv: HTMLCanvasElement) => {
+    renderer.render(data).then((canv: HTMLCanvasElement) => {
         document.body.appendChild(canv);
     });
 });
