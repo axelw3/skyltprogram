08b1ce6c98bf3223594876d86fb790c8b11f5dbd
axelw3 <axelw3@outlook.com>
2025-05-20 01:49:49 +0200

    Integrera renderingsfunktionen i huvuddokumentet

--- src/render.ts
+++ src/render.ts
@@ -1,5 +1,48 @@
 import { SignRenderer } from "../packages/skyltrendering/src/browser.js";
+import { SignElementOptions } from "../packages/skyltrendering/src/typedefs.js";
 import CONFIG from "./config.js"
+
+let getJSON = (url: string) => {
+    return new Promise((resolve, _) => {
+        let req = new XMLHttpRequest();
+        req.addEventListener("load", () => {
+            resolve(JSON.parse(req.responseText));
+        });
+        req.open("GET", url);
+        req.send();
+    });
+};
+
+let data_input: any = document.getElementById("input"), fr: any = document.getElementById("render"),
+    width_input: any = document.querySelector("input#width"), height_input: any = document.querySelector("input#height");
+
+function updateDisplay(data: SignElementOptions){
+    let w = parseInt(width_input.value),
+        h = parseInt(height_input.value);
+
+    fr.replaceChildren();
+    renderer.render(data, (!isNaN(w) && !isNaN(h) && w > 0 && h > 0) ? [w, h] : undefined).then((canv: HTMLCanvasElement) => {
+        fr.appendChild(canv);
+    });
+}
+
+data_input.addEventListener("change", () => {
+    try{
+        let parsed: SignElementOptions = JSON.parse(data_input.value);
+        data_input.value = JSON.stringify(parsed, null, 4);
+        updateDisplay(parsed);
+    }catch(e){
+        console.error(e);
+    }
+});
+
+width_input.addEventListener("change", () => {
+    updateDisplay(JSON.parse(data_input.value));
+});
+
+height_input.addEventListener("change", () => {
+    updateDisplay(JSON.parse(data_input.value));
+});
 
 let renderer = new SignRenderer(CONFIG);
 
@@ -13,16 +56,28 @@ Promise.all([
         "font/TRATEXPOSVERSAL-POSVERSAL.otf"
     )
 ]).then(() => {
-    let url = new URL(window.location.href);
-    let sp = url.searchParams;
-    if(!sp.has("data")) return;
+    console.log("OK");
+});
 
-    let w = sp.get("width"),
-        h = sp.get("height");
+getJSON("templates.json").then((templateData: any) => {
+    let templateSelect: any = document.getElementById("selectTemplate");
 
-    let data = JSON.parse(sp.get("data") || "");
-
-    renderer.render(data, (w !== null && h !== null) ? [parseInt(w), parseInt(h)] : undefined).then((canv: HTMLCanvasElement) => {
-        document.body.appendChild(canv);
+    templateData.forEach((template: any, i: number) => {
+        let option_el = document.createElement("option");
+        option_el.appendChild(document.createTextNode(template.name));
+        option_el.setAttribute("value", i.toString(10));
+        templateSelect.appendChild(option_el);
     });
+
+    templateSelect.value = data_input.value.length < 6 ? 0 : (-1);
+
+    templateSelect.addEventListener("change", () => {
+        let i = parseInt(templateSelect.value);
+
+        if(i >= 0) data_input.value = JSON.stringify(templateData[i].json, null, 4);
+
+        data_input.dispatchEvent(new Event("change"));
+    });
+
+    templateSelect.dispatchEvent(new Event("change"));
 });
